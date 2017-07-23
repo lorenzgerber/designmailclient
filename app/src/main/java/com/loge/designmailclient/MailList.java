89590840b37873a7a36e8243bba439aac6f8a83b
Lorenz Gerber <genfys@gmail.com>
2017-07-23 08:54:55 +0200

    initial checkin

---
+++ app/src/main/java/com/loge/designmailclient/MailList.java
@@ -0,0 +1,13 @@
+package com.loge.designmailclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MailList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_list);
+    }
+}
