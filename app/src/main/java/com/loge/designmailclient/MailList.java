de81d8a125ed455d19293376dd95ff3eaff3d279
Lorenz Gerber <genfys@gmail.com>
2017-07-24 22:40:55 +0200

    finished design

--- app/src/main/java/com/loge/designmailclient/MailList.java
+++ app/src/main/java/com/loge/designmailclient/MailList.java
@@ -1,9 +1,13 @@
 package com.loge.designmailclient;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+
 
 public class MailList extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -14,6 +18,16 @@ public class MailList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_list);
+
+        // Find the toolbar view inside the activity layout
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        //setSupportActionBar(toolbar);
+
+
+
+
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
@@ -29,5 +43,30 @@ public class MailList extends AppCompatActivity {
         mAdapter = new MyAdapter(mails.getMails());
         mRecyclerView.setAdapter(mAdapter);
 
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        actionBarDrawerToggle.syncState();
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+
 }
